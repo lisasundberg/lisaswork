294457aac6c45505b1d1cda06c6fce10d0060cbf
Lisa <hello@lisasundberg.com>
2020-09-10 15:53:15 +0200

    Lintlintlitnlitnlinterrrrrs

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -3,14 +3,20 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: ["plugin:react/recommended", "airbnb"],
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
     ecmaVersion: 12,
-    sourceType: "module",
+    sourceType: 'module',
   },
-  plugins: ["react"],
-  rules: {},
+  plugins: [
+    'react',
+  ],
+  rules: {
+  },
 };
