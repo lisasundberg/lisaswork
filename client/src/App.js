002b296329156ac0783bca600dfe26f6bd23c1a5
Lisa <hello@lisasundberg.com>
2020-10-06 17:16:48 +0200

    Sett upp sass

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-// import './App.css';
+import './App.scss';
 
+import Header from './components/Header';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 import Register from './components/auth/Register';
@@ -12,6 +13,7 @@ function App() {
 		<Router>
 			<div className="App">
 				<Navbar />
+				<Header />
 				<Route exact path="/" component={Landing} />
 				<Route exact path="/register" component={Register} />
 				<Route exact path="/login" component={Login} />
---
+++ client/src/components/Header/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './index.scss';
+
+const Header = () => {
+	return (
+		<header className="header">
+			<h1>Lisa Sundberg</h1>
+		</header>
+	);
+};
+export default Header;
